9e079604d54871dd01927b2f3b379751166e8477
arthur <arthur.mota005@gmail.com>
2020-03-21 10:19:29 -0300

    add 1050

--- 1050.py
+++ 1050.py
@@ -6,7 +6,13 @@ Code your solution here
 Escriba su solución aquí
 '''
 
+DDD = {'61': 'Brasilia', '71': 'Salvador', '11': 'Sao Paulo', '21': 'Rio de Janeiro',
+       '32': 'Juiz de Fora', '19': 'Campinas', '27': 'Vitoria', '31': 'Belo Horizonte'}
+entrada = input()
+entradaInt =  str(entrada)
 
-
-
+if entradaInt in DDD:
+    print(DDD.get(entradaInt))
+else:
+    print("DDD nao cadastrado")
 
